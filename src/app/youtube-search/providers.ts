ce92355f6847bbe8e06ff4bfddcac9379e964e95
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-26 12:25:46 +0200

    refactoring response in YouTubeService

--- src/app/youtube-search/providers.ts
+++ src/app/youtube-search/providers.ts
@@ -14,9 +14,11 @@ import {
 } from "@angular/core";
 
 import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+
 import { SearchResult } from './search-result-model';
 
-let YOUTUBE_API_KEY: string = "AIzaSyBSM04Dv_7t-kbi43yCgYQ96PpYoZarzhk";
+let YOUTUBE_API_KEY: string = "AIzaSyAyMONPDO7l3sxN0kDoVtfq1w_lW2fGQeY";
 let YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
 
 export var serviceInjectables: Array<any> = [
@@ -50,8 +52,16 @@ export class YouTubeService {
 
     return this.http.get(queryUrl)
       .map((res: Response) => {
-        return (<any>res.json()).items.map( (item: any) => {
-          console.log('raw item: ', item)
+        let searchList: any = res.json();
+        console.log(searchList);
+        return searchList.items.map( (item: any) => {
+          // console.log('raw item: ', item)
+          return new SearchResult({
+            id: item.id.videoId,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnailUrl: item.snippet.thumbnails.high.url
+          })
         })
     })
   }
