6acb11c364b7ac57fc630165e6cefb9b7ea7aa2d
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-27 14:29:25 +0200

    added function makeHeaders in complexHttp component

--- src/app/complex-Http-component.ts
+++ src/app/complex-Http-component.ts
@@ -3,7 +3,12 @@
  */
 
 import { Component } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import {
+  Http,
+  Response,
+  Headers,
+  RequestOptions
+} from "@angular/http";
 
 @Component({
   selector: "complex-http",
@@ -11,6 +16,7 @@ import { Http, Response } from "@angular/http";
     <h1>Complex request</h1>
     <button type="button" class="btn btn-primary search" (click)="makePost()">Make Post</button>
     <button type="button" class="btn btn-primary search" (click)="deletePost()">Delete Post</button>
+    <button type="button" class="btn btn-primary search" (click)="makeHeaders()">Make Req w/headers</button>
     <div *ngIf="loading">Loading...</div>
     <pre>{{ data | json }}</pre>
 `
@@ -42,6 +48,24 @@ export class ComplexHttp {
   deletePost(): void{
     this.loading = true;
     this.http.delete('http://jsonplaceholder.typicode.com/posts/1')
+      .subscribe((res: Response) => {
+        this.data = res.json();
+        this.loading = false;
+        }
+      )
+  }
+
+  makeHeaders(): void{
+
+    this.loading = true;
+
+    let headers: Headers = new Headers();
+    headers.append('X-API-TOKEN', 'XXXYYYZZZ');
+
+    let oprs: RequestOptions = new RequestOptions();
+    oprs.headers = headers
+
+    this.http.get('http://jsonplaceholder.typicode.com/posts/1')
       .subscribe((res: Response) => {
         this.data = res.json();
         this.loading = false;
